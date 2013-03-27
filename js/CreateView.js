b4557f8c29984812b0bbb6a750581b3758bc68cb
Nick Bury <nmbury@gmail.com>
2013-03-27 16:19:58 -0400

    Add rendering of form elements for creating a route

--- js/CreateView.js
+++ js/CreateView.js
@@ -1,26 +1,52 @@
 /*jslint indent:4*/
-/*global window, console, $, HandleBars */
+/*global window, console, $, Handlebars, alert */
 
 var CreateView = function () {
 
+    var formCount = 0;
+
     this.render = function () {
-
+        var self = this;
+        var template = Handlebars.compile($("#create-form-tpl").html());
+        $(".create-render").html(template(formCount)).trigger("create");
+        $("#add-btn-" + formCount).on("tap", function (event) {
+            var eventID = formCount;
+            formCount++;
+            self.addLocation(eventID);
+        });
+        $("#rem-btn-" + formCount).on("tap", function (event) {
+            if (formCount === 0) {
+                alert("You need at least one place to go!");
+            } else {
+                var toBeRemoved = $(this).attr("id").match(/\d/);
+                $("#form-" + toBeRemoved).remove();
+                formCount--;
+            }
+        });
     };
 
-    this.addLocation = function (event) {
-
-    };
-
-    this.removeLocation = function (event) {
-
-    };
-
-    this.save = function () {
-
+    this.addLocation = function (eventID) {
+        var self = this;
+        var template = Handlebars.compile($("#create-form-tpl").html());
+        $(".create-render").append(template(formCount)).trigger("create");
+        $("#add-btn-" + formCount).on("tap", function (event) {
+            var eventID = formCount;
+            formCount++;
+            self.addLocation(eventID);
+        });
+        $("#rem-btn-" + formCount).on("tap", function (event) {
+            if (formCount === 0) {
+                alert("You need at least one place to go!");
+            } else {
+                var toBeRemoved = $(this).attr("id").match(/\d/);
+                $("#form-" + toBeRemoved).remove();
+                formCount--;
+            }
+        });
     };
 
     this.initialize = function () {
-
+        this.render();
     };
 
     this.initialize();
